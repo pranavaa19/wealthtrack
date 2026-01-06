25c60617f78077b618f06ba59f972b6f183a062e
B.Pranavaa <babupranavaa@gmail.com>
2026-01-06 20:15:10 +0530

    feat: move firebase config to environment variables

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDmF4gu602nfOazjim7105BAAcc5f5HgoI",
-    authDomain: "antigravitywealth.firebaseapp.com",
-    projectId: "antigravitywealth",
-    storageBucket: "antigravitywealth.firebasestorage.app",
-    messagingSenderId: "881298435552",
-    appId: "1:881298435552:web:886621b3532711f9478f85",
-    measurementId: "G-P8PVXECKWQ"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
